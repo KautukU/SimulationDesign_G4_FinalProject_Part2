f7e42d8f648174ebbdb4e4ef1281859bfa108fea
Kautuk <Kautuk.u@gmail.com>
2022-04-11 17:03:32 -0400

    Not moving agent Fixed

--- Assets/RollerAgent.cs
+++ Assets/RollerAgent.cs
@@ -38,4 +38,38 @@ public class RollerAgent : Agent
         sensor.AddObservation(rBody.velocity.x);
         sensor.AddObservation(rBody.velocity.z);
     }
+
+    public float forceMultiplier = 10;
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition,
+        Target.localPosition);
+        // Reached target
+        
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            Debug.Log(string.Format("too close"));
+            EndEpisode();
+        }
+        // Fell off platform
+        else if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+    }
+
 }
