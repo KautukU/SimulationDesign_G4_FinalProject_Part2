7a7559eeced43e70f7440980cf293543e0771b65
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2022-04-12 00:10:22 -0400

    Timer implemented

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public float timeRemaining = 70;
+    public Text timeText;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,22 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            DisplayTime(timeRemaining);
+        }
+        else
+        {
+            Debug.Log("Time has run out!");
+            timeRemaining = 0;
+        }
+    }
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        float milliSeconds = (timeToDisplay % 1) * 1000;
+        timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliSeconds);
     }
 }
